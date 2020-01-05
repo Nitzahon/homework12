4fa0dd71c7b0118723b4fc231036c38ca065a536
Nitzahon <nadavga@gmail.com>
2020-01-05 18:54:12 +0200

    fixed App.js as rcc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+
+
+
+import React, { Component } from 'react'
 // import logo from "./logo.svg";
 import "./App.css";
 //import Post from "./components/Post.js";
@@ -7,20 +10,17 @@ import Post from "./components/Post.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-import React, { Component } from 'react'
-
-state ={list: [
-  { title: "The Force Awakens", info: "7" },
-  { title: "The Last Jedi", info: "8" },
-  { title: "The Rise of Skywalker", info: "AWWWW YEAH!!!!!"}
-]}
 export default class App extends Component {
+  state ={list: [
+    { title: "The Force Awakens", info: "7" },
+    { title: "The Last Jedi", info: "8" },
+    { title: "The Rise of Skywalker", info: "AWWWW YEAH!!!!!"}
+  ]}
   render() {
     return (
       <div>
               <Register />
-      {posts.map((element)=>{
+      {this.state.list.map((element)=>{
         return <Post title={element.title} info={element.info}/>
       })}
       </div>
