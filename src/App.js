e25c6c91c5bb21b5036499900a2a7e611c9b6ab0
Nitzahon <nadavga@gmail.com>
2020-01-05 17:41:00 +0200

    changed app to rfc

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import Register from "./components/Register.js";
 import Post from "./components/Post.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  let posts = [
-    { title: "The Force Awakens", info: "7" },
-    { title: "The Last Jedi", info: "8" },
-    { title: "The Rise of Skywalker", info: "AWWWW YEAH!!!!!"}
-  ];
-  return (
-    <div className="App">
 
 
+import React, { Component } from 'react'
 
-      <Register />
+state ={list: [
+  { title: "The Force Awakens", info: "7" },
+  { title: "The Last Jedi", info: "8" },
+  { title: "The Rise of Skywalker", info: "AWWWW YEAH!!!!!"}
+]}
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+              <Register />
       {posts.map((element)=>{
         return <Post title={element.title} info={element.info}/>
       })}
-
-    </div>
-  );
+      </div>
+    )
+  }
 }
-
-export default App;
